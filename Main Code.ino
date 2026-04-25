c324024d029e5eb77234874e2d72aeb5155a937b
Muhammad Ma'ruf A. <232618709+MMarufAwaludien@users.noreply.github.com>
2026-04-25 10:47:27 +0700

    Create Main Code.ino

---
+++ Main Code.ino
@@ -0,0 +1,67 @@
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
+
+char password[] = "1234";
+char inputCode[5]; // Kapasitas 5 (4 angka + 1 null terminator)
+int inputCount = 0;
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; 
+byte colPins[COLS] = {5, 4, 3, 2}; 
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+LiquidCrystal_I2C lcd(32, 16, 2); 
+Servo myservo;
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  myservo.attach(10);
+  myservo.write(0); 
+  lcd.print("Pin :");
+}
+
+void loop() {
+  char key = keypad.getKey();
+
+  if (key) {
+    inputCode[inputCount] = key;
+    lcd.setCursor(inputCount, 1);
+    lcd.print('*');
+    inputCount++;
+
+    if (inputCount == 4) {
+      inputCode[inputCount] = '\0'; // PENTING: Menutup string agar strcmp bekerja
+      delay(500);
+      
+      if (strcmp(inputCode, password) == 0) {
+        lcd.clear();
+        lcd.print("Akses Diterima");
+        myservo.write(90); 
+        delay(3000);
+        myservo.write(0);  
+        resetLcd();
+      } else {
+        lcd.clear();
+        lcd.print("Password Salah!");
+        delay(2000);
+        resetLcd();
+      }
+      inputCount = 0; // Reset hitungan
+    }
+  }
+}
+
+void resetLcd() {
+  lcd.clear();
+  lcd.print("Pin :");
+  inputCount = 0; // Memastikan counter benar-benar nol
+}
